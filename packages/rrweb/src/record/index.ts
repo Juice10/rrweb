7490071e7e94daf5a26ccc1b6b37a43f36ee58c3
Justin Halsall <Juice10@users.noreply.github.com>
2023-02-09 15:58:23 +0100

    Remove logging

--- packages/rrweb/src/record/index.ts
+++ packages/rrweb/src/record/index.ts
@@ -110,8 +110,6 @@ function record<T = eventWithTime>(
     sampling.mousemove = mousemoveWait;
   }
 
-  console.log('recorder setup');
-
   // reset mirror in case `record` this was called earlier
   mirror.reset();
 
