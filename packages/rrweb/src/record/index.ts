ebd13a741f6a6423270cc7e07a5aba61d81393e5
Justin Halsall <Juice10@users.noreply.github.com>
2023-02-09 14:11:58 +0100

    Add logging

--- packages/rrweb/src/record/index.ts
+++ packages/rrweb/src/record/index.ts
@@ -109,6 +109,8 @@ function record<T = eventWithTime>(
   if (mousemoveWait !== undefined && sampling.mousemove === undefined) {
     sampling.mousemove = mousemoveWait;
   }
+    
+  console.log('recorder setup');
 
   // reset mirror in case `record` this was called earlier
   mirror.reset();
